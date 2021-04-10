21b7808d1e317a41159c15fa5075cab04cec514e
Sinem Guner <sinem_guner95@outlook.com>
2021-04-11 00:14:12 +0300

    Homework1.1 solved

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,15 @@
-#Explain your work
-
 #Question 1
-for x in range(a):
-	print(a)
+list = [1,2,3,4,5,6,"Global","AI","Hub"]
+
+firstHalf = []
+lastHalf =[]
+
+for i in range(len(list)//2) :
+	firstHalf.append(list[i])
+for j in range(len(list)//2,len(list)):
+	lastHalf.append(list[j])
+
+lastHalf = lastHalf + firstHalf
+print(lastHalf)
+
+#Question 2
