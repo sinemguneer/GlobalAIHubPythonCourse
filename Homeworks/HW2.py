0d945063546e90233a9e4fc5c602e06bc71a2949
Sinem Guner <sinem_guner95@outlook.com>
2021-04-11 01:19:37 +0300

    Homework2 solved

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,17 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+dict = {"global" :"123","admin":"admin"}
+username = ""
+password = ""
+
+def check(username, password):
+	username = input("Username :")
+	password = input("Password :")
+
+	if username in dict.keys() and password in dict.values():
+		print("Successfully Login!")
+	else:
+		print("Login Failed!!")
+
+check(username,password)
